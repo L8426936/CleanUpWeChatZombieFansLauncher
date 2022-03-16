4b6beb3aa3ff3d47552a92292f4c664b3d458b78
L8426936 <1278764240@qq.com>
2022-03-16 12:48:20 +0800

    修复bug

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
         </vertical>
     );
 
+    let keep = true;
     let base_path = files.getSdcardPath() + "/Android/data/" + context.getPackageName() + "/CleanUpWeChatZombieFans/";
     let base_url = "https://gitee.com/L8426936/CleanUpWeChatZombieFans/raw/master/";
     // 本地测试使用
@@ -18,6 +19,7 @@
      * 初始化配置
      */
     function init() {
+        ui.try_download.setEnabled(false);
         if (files.exists(base_path + "main.js")) {
             engines.execScriptFile(base_path + "main.js", {path: base_path});
             engines.myEngine().forceStop();
@@ -26,6 +28,10 @@
         }
     }
     init();
+    
+    ui.emitter.on("back_pressed", () => {
+        keep = false;
+    });
 
     ui.try_download.on("click", () => {
         downloadFile();
@@ -33,55 +39,49 @@
 
     function downloadFile() {
         ui.download_message.setText("等待脚本下载完成");
-        ui.try_download.setEnabled(false);
         http.get(base_url + "config/files_md5.json", {}, (res, err) => {
             try {
                 if (err || res["statusCode"] != 200) {
                     ui.run(function () {
-                        ui.download_message.setText(err["message"] || res["statusCode"] + " " + res["statusMessage"] || "下载失败");
                         ui.try_download.setEnabled(true);
+                        if (err) {
+                            ui.download_message.setText(err["message"]);
+                        } else {
+                            ui.download_message.setText(res["statusCode"] + " " + res["statusMessage"]);
+                        }
                     });
                 } else {
                     let files_md5 = res.body.json();
-                    let completed_all_file = true, max_progress = 0, current_progress = 0;
+                    let max_progress = 0, current_progress = 0;
                     for (let key in files_md5) {
                         max_progress += files_md5[key]["size"];
                     }
                     for (let key in files_md5) {
                         let response = http.get(base_url + key);
-                        if (response["statusCode"] == 200) {
+                        if (response && response["statusCode"] == 200) {
                             current_progress += files_md5[key]["size"];
                             ui.run(function () {
                                 ui.progressbar.setProgress(current_progress * 100 / max_progress);
                             });
-                            files.ensureDir(base_path + ".download_files/" + key);
-                            files.write(base_path + ".download_files/" + key, response.body.string());
+                            files.ensureDir(base_path + ".cache/" + key);
+                            files.write(base_path + ".cache/" + key, response.body.string());
                         } else {
-                            completed_all_file = false;
-                            break;
+                            throw new Error("下载失败");
                         }
                     }
-                    if (completed_all_file) {
-                        for (let key in files_md5) {
-                            if (!files.copy(base_path + ".download_files/" + key, base_path + key)) {
-                                completed_all_file = false;
-                                break;
-                            }
+                    for (let key in files_md5) {
+                        files.ensureDir(base_path + key);
+                        if (!files.copy(base_path + ".cache/" + key, base_path + key)) {
+                            throw new Error("复制失败");
                         }
                     }
-                    files.removeDir(base_path + ".download_files");
-                    if (completed_all_file) {
-                        ui.run(function () {
-                            ui.download_message.setText("下载完成");
-                        });
+                    ui.run(function () {
+                        ui.download_message.setText("下载完成");
+                    });
+                    if (keep) {
                         engines.execScriptFile(base_path + "main.js", {path: base_path});
-                        engines.myEngine().forceStop();
-                    } else {
-                        ui.run(function () {
-                            ui.download_message.setText("下载失败");
-                            ui.try_download.setEnabled(true);
-                        });
                     }
+                    engines.myEngine().forceStop();
                 }
             } catch (e) {
                 ui.run(function () {
@@ -89,6 +89,7 @@
                     ui.try_download.setEnabled(true);
                 });
             }
+            files.removeDir(base_path + ".cache");
         });
     }
 })();
